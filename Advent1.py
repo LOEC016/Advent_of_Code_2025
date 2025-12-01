135dc1ff9d3215ca104d9cb4d40aac4278751810
FiFi <vivienne.loe@gmail.com>
2025-12-01 16:37:50 +0100

    Add Part 2

--- Advent1.py
+++ Advent1.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-position = 50
+position = 50 #start
 counter = 0 
 
 df = pd.read_csv(r".venv\input.txt", header=None, dtype=str)
 
 for entry in df[0]:
     entry = entry.strip()
-    direction = entry[0]
+    direction = entry[0] # L or R
     spins = int(entry[1:])
 
     if direction == "R":
@@ -18,31 +18,33 @@ for entry in df[0]:
     if position == 0:
         counter += 1
 
-print("Part 1: " +  str(counter))
+print("Code part 1: " +  str(counter))
+#END PART ONE
+
+df = pd.read_csv(r".venv\input.txt", header=None, dtype=str)
+position = 50 #start
+counter = 0 #reset
+dials = 100  # 0..99
 
 for entry in df[0]:
-    entry = entry.strip()
-    direction = entry[0]
-    spins = int(entry[1:])
+    # Delete possible whitespaces
+    entry = entry.strip() 
+     # L or R
+    direction = entry[0] 
+   
+    raw_spins = int(entry[1:]) # spins from input
+    # (positiv) +spins = right; (negativ) -spins = left
+    spins = raw_spins if direction == "R" else -raw_spins 
 
-    if direction == "R":
-        updated_position = (position + spins) % 100
-        if position < updated_position:
-            if position < 0 <= updated_position:
-                counter += 1
-        else:
-            full_rounds = (position + spins) // 100
-            counter += full_rounds
-        position = updated_position
+    new_position = (position + spins) % dials
+    if spins > 0:
+        passed_zeros = ((position + spins) // dials) - (position // dials)
+    else:
+        passed_zeros = ((position - 1) // dials) - ((position + spins - 1) // dials)
 
-    elif direction == "L":
-        updated_position = (position - spins) % 100
-        if position > updated_position:
-            counter += 1
-        else:
-            full_rounds = (spins - position - 1) // 100 + 1 if spins > position else 0
-            counter += full_rounds
-        position = updated_position
-    
-print(full_rounds)
+    counter += passed_zeros
 
+
+    position = new_position
+
+print("Code part 2: " + str(counter))
