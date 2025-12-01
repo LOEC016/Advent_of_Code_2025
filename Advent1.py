cd989ad83d85b5831f97e6d15e443d7e79392275
FiFi <vivienne.loe@gmail.com>
2025-12-01 15:52:18 +0100

    Init mate

--- Advent1.py
+++ Advent1.py
@@ -3,7 +3,7 @@ import pandas as pd
 position = 50
 counter = 0 
 
-df = pd.read_csv(r"Z:\Bot\Python\.venv\input.txt", header=None, dtype=str)
+df = pd.read_csv(r".venv\input.txt", header=None, dtype=str)
 
 for entry in df[0]:
     entry = entry.strip()
