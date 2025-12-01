33eeca0af0c8530801035d19b40a65d7eb49968d
FiFi <vivienne.loe@gmail.com>
2025-12-01 15:48:17 +0100

    Init mate

---
+++ Advent1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+position = 50
+counter = 0 
+
+df = pd.read_csv(r"Z:\Bot\Python\.venv\input.txt", header=None, dtype=str)
+
+for entry in df[0]:
+    entry = entry.strip()
+    direction = entry[0]
+    spins = int(entry[1:])
+
+    if direction == "R":
+        position = (position + spins) % 100
+    elif direction == "L":
+        position = (position - spins) % 100
+
+    if position == 0:
+        counter += 1
+
+print(counter)
