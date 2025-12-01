ac27b578d6657fa958ac9dcad912ff8bc907a12a
FiFi <vivienne.loe@gmail.com>
2025-12-01 16:01:35 +0100

    Fix String issue

--- Advent1.py
+++ Advent1.py
@@ -18,6 +18,31 @@ for entry in df[0]:
     if position == 0:
         counter += 1
 
-print("Part 1: " + counter)
+print("Part 1: " +  str(counter))
 
+for entry in df[0]:
+    entry = entry.strip()
+    direction = entry[0]
+    spins = int(entry[1:])
+
+    if direction == "R":
+        updated_position = (position + spins) % 100
+        if position < updated_position:
+            if position < 0 <= updated_position:
+                counter += 1
+        else:
+            full_rounds = (position + spins) // 100
+            counter += full_rounds
+        position = updated_position
+
+    elif direction == "L":
+        updated_position = (position - spins) % 100
+        if position > updated_position:
+            counter += 1
+        else:
+            full_rounds = (spins - position - 1) // 100 + 1 if spins > position else 0
+            counter += full_rounds
+        position = updated_position
+    
+print(full_rounds)
 
